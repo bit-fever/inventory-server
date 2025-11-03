7caaf91a1917542ec0540b7319edd6bc614db655
acarboni <andrea.carboni71@gmail.com>
2025-11-03 18:27:15 +0100

    Renamed platform to tradalia (final fixes)

--- certificate/generate-certificate.sh
+++ certificate/generate-certificate.sh
@@ -4,7 +4,7 @@
 openssl req -newkey rsa:2048 -nodes -days 9999 -x509 -keyout ca.key -out ca.crt -subj "/CN=*"
 
 #--- Generate agent key & cert request
-openssl req -newkey rsa:2048 -nodes -keyout agent.key -out agent.csr -subj "/C=EU/ST=Italy/L=Rome/O=BitFever/OU=BitFeverAgent/CN=*"
+openssl req -newkey rsa:2048 -nodes -keyout agent.key -out agent.csr -subj "/C=EU/ST=Italy/L=Rome/O=Tradalia/OU=TradaliaAgent/CN=*"
 
 #--- Sign agent certificate with CA
 openssl x509 -req -days 9999 -sha256 -in agent.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out agent.crt \
